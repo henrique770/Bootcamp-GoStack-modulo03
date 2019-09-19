fd2c22a4f0abf04a458b5a6e4ba2d62735382da8
Henrique <henrique.1360@gmail.com>
2019-09-19 20:34:05 -0300

    modulo03 | Continuando API do GoBarber

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'aa9cbfc7ee6aa7e945e0765dc15729cb',
+  expiresIn: '7d',
+};
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '533befc52cfba9',
+    pass: '3a5c85242b80fa',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
